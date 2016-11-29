34845ac890cbecb4f2f1fcb087016a87ae402380
sp41mer <sp41mer@yahoo.com>
2016-11-29 15:04:07 +0300

    swal added

--- proj_new/js/main.js
+++ proj_new/js/main.js
@@ -2,13 +2,24 @@
  * Created by danilakimov on 18.11.16.
  */
 
-$( ".show__marks" ).on("click", function() {
-  $(this).parent().find( ".marks__all" ).toggle();
+$(".show__marks").on("click", function () {
+    $(this).parent().find(".marks__all").toggle();
 });
 
-$( ".blocks" ).on("click", function() {
-  window.location = $(this).find( "a" ).attr('href');
+$(".blocks").on("click", function () {
+    window.location = $(this).find("a").attr('href');
 });
 
+$('.dropdown-toggle').dropdown();
 
-$('.dropdown-toggle').dropdown()
+$('.js__mark_submit').click(function (e){
+    e.preventDefault();
+    var newThis = $(this);
+    swal({
+        title: "Оценка поставлена",
+        text: "Оценка успешно поставлена",
+        type: "success",
+    }, function () {
+        window.location = newThis.attr('href');
+    });
+});
